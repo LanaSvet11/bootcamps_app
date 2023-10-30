06e3e78fd0bc4f2e7842aea5c55576c2a2d7ab4e
lana Holovatenko <lholovatenko@LABA.PSWAS.LOCAL>
2023-10-30 16:03:00 -0700

    first commit

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import './index.css'
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="/">Camps</Link>
+      <Link to="/camps/new">Make a new camp</Link>
+    </div>
+  )
+}
+
+export default Navbar;
